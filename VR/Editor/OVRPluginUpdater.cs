acb9d507028b862fd1ec31e767e93946a4306167
Felix Herbst <herbst@prefrontalcortex.de>
2020-09-25 13:48:17 +0200

    fix plugin updater paths

--- VR/Editor/OVRPluginUpdater.cs
+++ VR/Editor/OVRPluginUpdater.cs
@@ -155,8 +155,9 @@ class OVRPluginUpdater
 			{
 				if (File.Exists(path))
 				{
-					string basePath = GetCurrentProjectPath();
-					string relPath = path.Substring(basePath.Length + 1);
+					string basePath = GetAbsolutePackagePath();
+					string localRelPath = path.Substring(basePath.Length + 1);
+					string relPath = GetPackagePath() + localRelPath;
 
 					PluginImporter pi = PluginImporter.GetAtPath(relPath) as PluginImporter;
 					if (pi != null)
@@ -300,6 +301,16 @@ class OVRPluginUpdater
 		return Directory.GetParent(Application.dataPath).FullName;
 	}
 
+	private static string GetAbsolutePackagePath()
+	{
+		return Path.GetDirectoryName(Path.GetFullPath("Packages/com.oculus.integration/package.json"));
+	}
+
+	private static string GetPackagePath()
+	{
+		return "Packages/com.oculus.integration/";
+	}
+	
 	private static string GetUtilitiesPluginRootPath()
 	{
 		return GetUtilitiesRootPath() + @"/Plugins";
@@ -429,8 +440,9 @@ class OVRPluginUpdater
 			{
 				if ((Directory.Exists(path)) || (File.Exists(path)))
 				{
-					string basePath = GetCurrentProjectPath();
-					string relPath = path.Substring(basePath.Length + 1);
+					string basePath = GetAbsolutePackagePath();
+					string localRelPath =  path.Substring(basePath.Length + 1);
+					string relPath = GetPackagePath() + localRelPath;
 					string relDisabledPath = relPath + GetDisabledPluginSuffix();
 
 					AssetDatabase.MoveAsset(relPath, relDisabledPath);
@@ -452,8 +464,9 @@ class OVRPluginUpdater
 
 			if ((Directory.Exists(path + GetDisabledPluginSuffix())) || (File.Exists(path + GetDisabledPluginSuffix())))
 			{
-				string basePath = GetCurrentProjectPath();
-				string relPath = path.Substring(basePath.Length + 1);
+				string basePath = GetAbsolutePackagePath();
+				string localRelPath = path.Substring(basePath.Length + 1);
+				string relPath = GetPackagePath() + localRelPath;
 				string relDisabledPath = relPath + GetDisabledPluginSuffix();
 
 				AssetDatabase.MoveAsset(relDisabledPath, relPath);
